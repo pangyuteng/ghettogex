84c30658b48ef14caae6b71a0dc5fbaea0913fa3
pangyuteng <pangyuteng@gmail.com>
2024-12-23 21:07:33 +0000

    added long

--- flask/utils/data_yahoo.py
+++ flask/utils/data_yahoo.py
@@ -1,5 +1,5 @@
-# from requests_ratelimiter import LimiterSession
-# prices = yf.download(tickers, period='5d', interval='1d', session=LimiterSession(per_second=3))
+import logging
+logger = logging.getLogger(__file__)
 import os
 import sys
 import json
@@ -10,6 +10,19 @@ import pandas as pd
 import yfinance as yf
 from requests_ratelimiter import LimiterSession
 
+MYFOLDER = os.environ.get("MYFOLDER","tmp")
+
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler(os.path.join(MYFOLDER,'log.txt'))
+fh.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+if False:
+    logger.addHandler(fh)
+logger.addHandler(ch)
 
 def get_option_chain(ticker,ticker_obj):
     mylist = []
@@ -33,23 +46,18 @@ def get_option_chain(ticker,ticker_obj):
 BTC_TICKER = "BTC-USD"
 INDEX_TICKER_LIST = ['SPY','QQQ','^SPX','^NDX']
 BTC_TICKER_LIST = ['^CBTX','^MBTX','ARKB','GBTC','IBIT','MSTR']
-# format
-# ticker/date_stamp/options_time_stamp.json
-# ticker/date_stamp/time_stamp/underlying.json
-MYFOLDER = os.environ.get("MYFOLDER","tmp")
 def main():
     now_utc = datetime.datetime.now(pytz.utc)
     eastern = pytz.timezone('US/Eastern')
     now_et = now_utc.astimezone(eastern)
-    print(now_et)
+    logger.info(str(now_et))
     date_stamp = datetime.datetime.strftime(now_et,'%Y-%m-%d')
-    #time_stamp = datetime.datetime.strftime(now_et,'%Y-%m-%d-%H-%M-%S-%Z')
-    time_stamp = datetime.datetime.strftime(now_et,'%Y-%m-%d-%H-%M-%Z')
+    time_stamp = datetime.datetime.strftime(now_et,'%Y-%m-%d-%H-%M-%Z') # '%Y-%m-%d-%H-%M-%S-%Z'
     ticker_list = [BTC_TICKER]
     ticker_list.extend(INDEX_TICKER_LIST)
     ticker_list.extend(BTC_TICKER_LIST)
     for ticker in ticker_list:
-        print(ticker,'underlying')
+        logger.info(f'{ticker} underlying')
         cache_folder = os.path.join(MYFOLDER,ticker,date_stamp)
         os.makedirs(cache_folder,exist_ok=True)
         json_file = os.path.join(cache_folder,f"underlying-{ticker}-{time_stamp}.json")
@@ -58,19 +66,15 @@ def main():
             info_dict = ticker_obj.info
             with open(json_file,'w') as f:
                 f.write(json.dumps(info_dict))
-            #price_df = ticker_obj.history(period='5d')
-            #last_price = price_df['Close'][-1]
-            #print(ticker,last_price)
         else:
-            print('found')
+            logger.info('underlying found')
 
-    print("----")
     ticker_list = []
     ticker_list.extend(INDEX_TICKER_LIST)
     ticker_list.extend(BTC_TICKER_LIST)
     tickers = yf.Tickers(ticker_list,session=LimiterSession(per_second=5))
     for ticker in ticker_list:
-        print(ticker,'options')
+        logger.info(f'{ticker} options')
         cache_folder = os.path.join(MYFOLDER,ticker,date_stamp)
         os.makedirs(cache_folder,exist_ok=True)
         csv_file = os.path.join(cache_folder,f"options-{ticker}-{time_stamp}.csv")
@@ -79,11 +83,10 @@ def main():
             df = get_option_chain(ticker,ticker_obj)
             df.to_csv(csv_file,index=False)
         else:
-            print('found')
+            logger.info('options found')
 
 if __name__== "__main__":
     while True:
         main()
-        print(datetime.datetime.now())
-        print('sleeping...')
+        logger.info('sleeping...')
         time.sleep(5)
