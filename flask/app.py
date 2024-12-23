3f49e7b598e96ae6b839d63aeb0941f7de8c7dd4
pangyuteng <pangyuteng@gmail.com>
2024-12-23 04:46:19 +0000

    wip

--- flask/app.py
+++ flask/app.py
@@ -1,3 +1,31 @@
+import os
+import sys
+import argparse
+from flask import (
+    Flask, request, render_template, Response,
+    redirect, url_for, jsonify,
+)
+
+
+app = Flask(__name__,
+    static_url_path='', 
+    static_folder='static',
+    template_folder='templates',
+)
+
+app.config["SECRET_KEY"] = "the random string"
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p","--port",type=int,default=5000)
+    args = parser.parse_args()
+    app.run(debug=True,host="0.0.0.0",port=args.port)
 
 
 
