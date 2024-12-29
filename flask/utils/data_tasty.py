dd8e93d2ffca8853afd9b2e1cb2f5ac37231c4f4
pangyuteng <pangyuteng@gmail.com>
2024-12-29 20:00:06 +0000

    wip wip update tastytrade

--- flask/utils/data_tasty.py
+++ flask/utils/data_tasty.py
@@ -161,12 +161,12 @@ class LivePrices:
         options = [o for o in chain[expiration]]
         # the `streamer_symbol` property is the symbol used by the streamer
         streamer_symbols = [o.streamer_symbol for o in options]
-
+        print("??oooooooooooooooooo?")
         streamer = await DXLinkStreamer(session)
+        print("??????????")
         # subscribe to quotes and greeks for all options on that date
         start_time = now_in_new_york()
         start_time = datetime.datetime(start_time.year,start_time.month,start_time.day,9,30,0)
-        start_time = datetime.datetime(2024,12,27,9,30,0)
         await streamer.subscribe_candle([ticker] + streamer_symbols, CANDLE_TYPE, start_time)
         await streamer.subscribe(Greeks, streamer_symbols)
         await streamer.subscribe(Profile, streamer_symbols)
