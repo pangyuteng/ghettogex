0e8ad8ec0214c7f05773643447667dd04df40772
pangyuteng <pangyuteng@gmail.com>
2024-12-31 18:18:49 +0000

    wip IV plotting

--- scratch/iv_plot.py
+++ scratch/iv_plot.py
@@ -2,18 +2,10 @@
 import os
 import sys
 import json
+import re
 import pathlib
 import pandas as pd
-
-
-CACHE_TASTY_FOLDER = "tmp"
-
-folder_list = os.listdir(CACHE_TASTY_FOLDER)
-print(folder_list)
-sys.exit(1)
-summary_folder = os.path.join(folder,'summary')
-timeandsale_folder = os.path.join(folder,'timeandsale')
-
+from tqdm import tqdm
 
 # sample event_symbol ".TSLA240927C105"
 PATTERN = r"\.([A-Z]+)(\d{6})([CP])(\d+)"
@@ -26,25 +18,48 @@ def parse_symbol(event_symbol):
     strike = float(matched.group(4))
     return ticker,expiration,contract_type,strike
 
-
-open_interest = None
-json_file_list = sorted([str(x) for x in sorted(pathlib.Path(summary_folder).rglob("*.json"))])
-for json_file in json_file_list:
-    with open(json_file,'r') as f:
+def process(json_file):
+    event_dict = {}
+    file_split = json_file.split("/")
+    tstamp, uid = file_split[-1].replace(".json","").split("-uid-")
+    event_type = file_split[-2]
+    streamer_symbol = file_split[-3]
+    with open(json_file, 'r') as f:
         content = json.loads(f.read())
-        #print(json_file,content['open_interest'])
-        open_interest = content['open_interest']
 
-mylist = []
-json_file_list = sorted([str(x) for x in sorted(pathlib.Path(timeandsale_folder).rglob("*.json"))])
-for json_file in json_file_list:
-    with open(json_file,'r') as f:
-        content = json.loads(f.read())
-        content['json_file']=json_file
-        content['open_interest']=open_interest
-        key_list = ['type','aggressor_side','size','open_interest','json_file']
-        item = {x:content[x] for x in key_list}
-        mylist.append(item)
-        print(json_file)
-df = pd.DataFrame(mylist)
-df.to_csv("ok.csv",index=False)
+    event_dict.update(content)
+    if streamer_symbol.startswith("."):
+        ticker,expiration,contract_type,strike = parse_symbol(streamer_symbol)
+        event_dict['ticker']=ticker
+        event_dict['expiration']=expiration
+        event_dict['contract_type']=contract_type
+        event_dict['strike']=strike
+    else:
+        event_dict['ticker']=streamer_symbol
+
+    event_dict["streamer_symbol"]=streamer_symbol
+    event_dict["event_type"]=event_type
+    event_dict["uid"]=uid
+    event_dict["tstamp"]=tstamp
+    return event_dict
+
+def main():
+
+    CACHE_TASTY_FOLDER = "tmp"
+    ticker = 'SPX'
+    root_folder = os.path.join(CACHE_TASTY_FOLDER,ticker)
+
+    json_file_list = [str(x) for x in pathlib.Path(root_folder).rglob("*.json")]
+    json_file_list = [x for x in json_file_list if 'greeks' in x]
+
+    print(len(json_file_list))
+
+    for json_file in tqdm(json_file_list):
+        myitem = process(json_file)
+        mylist.append(myitem)
+
+    df = pd.DataFrame(mylist)
+    df.to_csv("ok.csv",index=False)
+
+if __name__ == "__main__":
+    main()
