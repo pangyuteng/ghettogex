93d624dd670e07a8b7d841ec62c1f8486ab9a36d
pangyuteng <pangyuteng@gmail.com>
2025-01-06 05:32:42 +0000

    wip worker logs

--- flask/worker.sh
+++ flask/worker.sh
@@ -4,10 +4,13 @@ export WORKERNAME=worker-$(openssl rand -hex 5)
 export LOGFILE=/tmp/workerlog-${WORKERNAME}.txt
 echo -n $WORKERNAME > /tmp/workername.txt
 
+
 celery -A tasks worker -Q default \
     --hostname=$WORKERNAME \
-    --logfile=$LOGFILE \
     --pidfile /tmp/celeryworker.pid \
     --pool=solo \
     --concurrency=1 -Ofair --max-tasks-per-child=1 \
     --prefetch-multiplier=1 --without-gossip --loglevel=INFO
+
+
+#    --logfile=$LOGFILE \
