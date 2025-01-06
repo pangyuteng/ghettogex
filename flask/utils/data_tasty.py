cc968bdb0eebf31cf7b5a8cd7537c4239ef82066
pangyuteng <pangyuteng@gmail.com>
2025-01-06 16:39:56 +0000

    see if 1s candle works

--- flask/utils/data_tasty.py
+++ flask/utils/data_tasty.py
@@ -126,7 +126,7 @@ def parse_symbol(event_symbol):
 # https://tastyworks-api.readthedocs.io/en/latest/data-streamer.html#advanced-usage
 # commit https://github.com/tastyware/tastytrade/blob/97e1bc6632cfd4a15721da816085eb906a02bcb0/docs/data-streamer.rst#L76
 # # interval '5s' '15s', '5m', '1h', '3d',
-CANDLE_TYPE = '1s'
+CANDLE_TYPE = 's'
 @dataclass
 class LivePrices:
     candle: dict[str, Candle]
