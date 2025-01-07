4e8c5d6f5f72f1568b6dabcb9ead26ccf90fe289
pangyuteng <pangyuteng@gmail.com>
2025-01-07 05:18:45 +0000

    added table event_agg

--- postgres/02-create-gex.sql
+++ postgres/02-create-gex.sql
@@ -9,16 +9,33 @@ CREATE TABLE IF NOT EXISTS gex_strike (
     UNIQUE (ticker, tstamp, strike)
 );
 
-
 CREATE TABLE IF NOT EXISTS gex_net (
     gex_net_id SERIAL PRIMARY KEY, 
     ticker text,
     tstamp TIMESTAMP,
-    price_open double precision,
-    price_high double precision,
-    price_low double precision,
-    price_close double precision,
+    spot_price numeric,
     naive_gex double precision,
     volume_gex double precision,
     UNIQUE (ticker, tstamp)
+);
+
+CREATE TABLE IF NOT EXISTS event_agg (
+    event_agg_id SERIAL PRIMARY KEY,
+    event_symbol text NOT NULL,
+    tstamp_et TIMESTAMP,
+    spot_price numeric,
+    open_interest double precision,
+    ask_price numeric,
+    bid_price numeric,
+    imp_volatility numeric,
+    delta double precision,
+    gamma double precision,
+    theta double precision,
+    rho double precision,
+    vega double precision,
+    ticker text,
+    expiration TIMESTAMP,
+    contract_type text,
+    strike double precision,
+    tstamp TIMESTAMP default (now() at time zone 'utc')
 );
