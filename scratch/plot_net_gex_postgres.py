8b6b6c4d1903b99774b00b22fdbb1be02d1f41e4
pangyuteng <pangyuteng@gmail.com>
2025-01-11 07:29:01 +0000

    wip gex mp4

--- scratch/plot_net_gex_postgres.py
+++ scratch/plot_net_gex_postgres.py
@@ -44,7 +44,7 @@ def plot_iv(ticker,day_stamp):
 
     print(df.columns)
     print(df.shape)
-    df = df[['strike','naive_gex','spot_price''tstamp']]
+    df = df[['strike','naive_gex','spot_price','tstamp']]
     df = df.dropna()
     print(df.shape)
 
