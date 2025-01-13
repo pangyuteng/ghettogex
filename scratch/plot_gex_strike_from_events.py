6a88eacf7eecf779f2f8158d3a0abcf01a037360
pangyuteng <pangyuteng@gmail.com>
2025-01-13 05:36:50 +0000

    wip gex

--- scratch/plot_gex_strike_from_events.py
+++ scratch/plot_gex_strike_from_events.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import pytz
+import json
 import datetime
 import numpy as np
 import pandas as pd
@@ -11,11 +12,9 @@ sys.path.append("/opt/fi/flask")
 from utils.postgres_utils import postgres_execute
 
 
-def main():
-    ticker = 'SPX'
-    day_stamp = '2025-01-07'
-    stime = time.time()
+def cache_data(ticker,day_stamp):
 
+    stime = time.time()
     postgres_query = "select * from candle where event_symbol = %s and tstamp::date = %s order by tstamp"
     postgres_args = (ticker,day_stamp)
     fetched = postgres_execute(postgres_query,postgres_args)
@@ -63,48 +62,45 @@ def main():
     reference_tstamp_list = pd.date_range(start=start_time,end=end_time,freq='s')
     print(len(reference_tstamp_list))
 
-    mydict = {
-        'uc': underlying_candle_df,
-        'c': candle_df,
-        'g': greeks_df,
-        's': summary_df,
-        't': timeandsale_df,
-    }
 
-    for k,v in mydict.items():
-        print(len(v))
-        v['tstamp_sec']=v.tstamp.apply(lambda x: x.replace(microsecond=0,tzinfo=utc))
-
+    stime = time.time()
     # get underlying price at 1sec freq
 
-    mergedf = pd.DataFrame([])
-    mergedf['tstamp_sec']=reference_tstamp_list
-    print(len(mergedf))
+    spot_df = pd.DataFrame([])
+    spot_df['tstamp_sec']=reference_tstamp_list
+    print(len(spot_df))
     uc = underlying_candle_df[underlying_candle_df.close > 0]
     uc = uc[['tstamp_sec','close']].groupby(['tstamp_sec']).last().reset_index()
     uc = uc.rename({'close':'spot_price'},axis=1)
-    mergedf = mergedf.merge(uc,how='left',on=['tstamp_sec'])
-    mergedf_null = mergedf.copy(deep=True)
-    nullcount_init = np.sum(mergedf.spot_price.isnull())
-    mergedf.spot_price = mergedf.spot_price.ffill()
-    nullcount_after_ffill = np.sum(mergedf.spot_price.isnull())
+    spot_df = spot_df.merge(uc,how='left',on=['tstamp_sec'])
+    spot_df_null = spot_df.copy(deep=True)
+    nullcount_init = np.sum(spot_df.spot_price.isnull())
+    spot_df.spot_price = spot_df.spot_price.ffill()
+    nullcount_after_ffill = np.sum(spot_df.spot_price.isnull())
     print(nullcount_init,nullcount_after_ffill,'!!')
-    prct_ffilled = 100*(nullcount_init-nullcount_after_ffill) / len(mergedf)
+    prct_ffilled = 100*(nullcount_init-nullcount_after_ffill) / len(spot_df)
     print('prct_ffilled',prct_ffilled)
 
 
-    summary_df = summary_df[['event_symbol','ticker','strike','contract_type','expiration','tstamp_sec','open_interest']]
-    summary_df = summary_df.groupby(['event_symbol','ticker','strike','contract_type','expiration','tstamp_sec']).last().reset_index()
+    print(spot_df.shape)
+    print(candle_df.shape)
+    print(greeks_df.shape)
+    print(summary_df.shape)
+    print(timeandsale_df.shape)
 
-    greeks_df = greeks_df[['event_symbol','tstamp_sec','price','volatility','delta','gamma','theta','rho','vega']]
-    greeks_df = greeks_df.groupby(['event_symbol','tstamp_sec']).last().reset_index()
+    gby_summary_df = summary_df[['event_symbol','ticker','strike','contract_type','expiration','open_interest']]
+    gby_summary_df = gby_summary_df.groupby(['event_symbol','ticker','strike','contract_type','expiration']).last().reset_index()
+    gby_summary_df['contract_type_int'] = gby_summary_df.contract_type.apply(lambda x: -1 if x == 'P' else 1)
+
+    gby_greeks_df = greeks_df[['event_symbol','price','volatility','delta','gamma','theta','rho','vega','tstamp_sec']]
+    gby_greeks_df = gby_greeks_df.groupby(['event_symbol','tstamp_sec']).last().reset_index()
 
     timeandsale_df['size_signed'] = timeandsale_df['size'].where(timeandsale_df.aggressor_side == 'BUY', other=-1*timeandsale_df['size'])
-    timeandsale_df = timeandsale_df[['event_symbol','size_signed','tstamp_sec']]
-    timeandsale_df = timeandsale_df.groupby(['event_symbol','tstamp_sec']).sum().reset_index()
-    
-    candle_df = candle_df[['event_symbol','tstamp_sec','open','high','low','close','volume','bid_volume','ask_volume']]
-    candle_df = candle_df.groupby(['event_symbol','tstamp_sec']).agg(
+    gby_timeandsale_df = timeandsale_df[['event_symbol','size_signed','tstamp_sec']]
+    gby_timeandsale_df = gby_timeandsale_df.groupby(['event_symbol','tstamp_sec']).sum().reset_index()
+
+    gby_candle_df = candle_df[['event_symbol','open','high','low','close','volume','bid_volume','ask_volume','tstamp_sec']]
+    gby_candle_df = gby_candle_df.groupby(['event_symbol','tstamp_sec']).agg(
         open=pd.NamedAgg(column="open", aggfunc="last"),
         high=pd.NamedAgg(column="high", aggfunc="last"),
         low=pd.NamedAgg(column="low", aggfunc="last"),
@@ -113,22 +109,54 @@ def main():
         bid_volume=pd.NamedAgg(column="bid_volume", aggfunc="sum"),
         ask_volume=pd.NamedAgg(column="ask_volume", aggfunc="sum"),
     ).reset_index()
-    print(candle_df.columns)
-    # summary OI is only at initial few seconds.... given we want to intraday OI....
-    # naive merge will not work.
 
-    mergedf = mergedf.merge(summary_df,how='left',on=['tstamp_sec'])
-    mergedf = mergedf.merge(greeks_df,how='left',on=['event_symbol','tstamp_sec'])
-    mergedf = mergedf.merge(timeandsale_df,how='left',on=['event_symbol','tstamp_sec'])
-    mergedf = mergedf.merge(candle_df,how='left',on=['event_symbol','tstamp_sec'])
-    mergedf['contract_type_int'] = mergedf.contract_type.apply(lambda x: -1 if x == 'P' else 1)
-    print(mergedf.columns)
-    print(mergedf.shape)
+    # each tstamp,event_symbol, strike*gmma*open_interest*spot_price*contract_type_int
+    pd.set_option('future.no_silent_downcasting', True)
+    mylist = []
+    for event_symbol in list(gby_summary_df.event_symbol.unique()):
+        su = gby_summary_df[gby_summary_df.event_symbol==event_symbol]
+        gk = gby_greeks_df[gby_greeks_df.event_symbol==event_symbol]
+        ts = gby_timeandsale_df[gby_timeandsale_df.event_symbol==event_symbol]
+        cd = gby_candle_df[gby_candle_df.event_symbol==event_symbol]
+        
+        ok = pd.merge(spot_df, su, how='cross')
+        ok = ok.merge(gk,how='left',on=['event_symbol','tstamp_sec'])
+        ok = ok.merge(ts,how='left',on=['event_symbol','tstamp_sec'])
+        ok = ok.merge(cd,how='left',on=['event_symbol','tstamp_sec'])
+        
+        # ffill gamma
+        ok.gamma = ok.gamma.ffill()
+        ok.close = ok.close.ffill()
+        
+        # fill 0s for timeandsale and candle volumes
+        ok.size_signed = ok.size_signed.fillna(value=0)
+        ok.bid_volume = ok.bid_volume.fillna(value=0)
+        ok.ask_volume = ok.ask_volume.fillna(value=0)
+        ok['oi_timeandsale'] = ok.size_signed
+        ok['oi_volume'] = ok.ask_volume-ok.bid_volume
+        try:
+            init_oi = float(ok.open_interest.to_list()[-1])
+        except:
+            init_oi = 0
+        ok.oi_timeandsale = ok.oi_timeandsale.cumsum().astype(float)+init_oi
+        ok.oi_volume = ok.oi_volume.cumsum().astype(float)+init_oi
+        mylist.append(ok.copy(deep=True))
+    foodf = pd.concat(mylist)
+
+    etime = time.time()
+    print(etime-stime)
+    return foodf
 
 if __name__ == "__main__":
-    main()
 
-
+    ticker = 'SPX'
+    day_stamp = '2025-01-08'
+    pq_file = "tmp/pg.parquet.gzip"
+    if not os.path.exists(pq_file):
+        foodf = cache_data(ticker,day_stamp)
+        foodf.to_parquet(pq_file,compression='gzip',index=False)
+    else:
+        foodf = pd.read_parquet(pq_file)
 
 """
 
