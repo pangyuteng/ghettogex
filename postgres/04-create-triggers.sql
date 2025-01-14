7a8721e2ae1970d0d364adefb44668ac92b5bb01
pangyuteng <pangyuteng@gmail.com>
2025-01-14 16:02:07 +0000

    added tstamp index

--- postgres/04-create-triggers.sql
+++ postgres/04-create-triggers.sql
@@ -11,3 +11,22 @@ CREATE TRIGGER underlying_trigger BEFORE insert or update on underlying FOR EACH
 CREATE TRIGGER watchlist_trigger BEFORE insert or update on watchlist FOR EACH ROW execute procedure notify_trigger();
 
 
+
+
+
+create index candle_tstamp_index on candle using brin (tstamp);
+create index event_tstamp_index on event using brin (tstamp);
+create index greeks_tstamp_index on greeks using brin (tstamp);
+create index profile_tstamp_index on profile using brin (tstamp);
+create index quote_tstamp_index on quote using brin (tstamp);
+create index summary_tstamp_index on summary using brin (tstamp);
+create index theoprice_tstamp_index on theoprice using brin (tstamp);
+create index timeandsale_tstamp_index on timeandsale using brin (tstamp);
+create index trade_tstamp_index on trade using brin (tstamp);
+create index underlying_tstamp_index on underlying using brin (tstamp);
+
+create index gex_strike_tstamp_index on gex_strike using brin (tstamp);
+create index gex_net_tstamp_index on gex_net using brin (tstamp);
+create index event_agg_tstamp_index on event_agg using brin (tstamp);
+
+  
