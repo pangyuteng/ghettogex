5e799733cb731c9f5f622c5243e9c28b451d9df5
pangyuteng <pangyuteng@gmail.com>
2025-01-25 20:31:06 +0000

    added notebook Dockerfile

--- flask/build_and_push.sh
+++ flask/build_and_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build -t fi-flask:latest .
+docker build -t fi-notebook:latest -f Dockerfile.nb .
+
+docker build -t fi-flask:latest -f Dockerfile .
 docker tag fi-flask:latest docker.io/pangyuteng/private:fi-flask-edge
 docker push docker.io/pangyuteng/private:fi-flask-edge
