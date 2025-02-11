fcd5070e66597bbc8186f2faac9294c68a8718d7
pangyuteng <pangyuteng@gmail.com>
2025-02-11 15:55:23 +0000

    added delete script

--- flask/utils/delete_postgres.py
+++ flask/utils/delete_postgres.py
@@ -3,7 +3,20 @@ from .postgres_utils import (
     postgres_execute, postgres_uri,
 )
 
-daystamp = sys.argv[1]
-# query_str = "delete from candle tstamp < %s"
-# query_args = (daystamp,)
-# postgres_execute(query_str,query_args,is_commit=True)
+
+if __name__ == "__main__":
+    daystamp = sys.argv[1]
+
+    table_list = 'timeandsale,summary,quote,greeks,event,candle,profile,theoprice,trade,underlying'.split(",")
+    for tablestr in table_list:
+        query_str = "delete from "+tablestr+" where tstamp < %s"
+        query_args = (daystamp,)
+        postgres_execute(query_str,query_args,is_commit=True)
+
+
+
+"""
+
+python -m utils.delete_postgres 2025-02-11
+
+"""
