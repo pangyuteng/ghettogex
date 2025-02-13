2481881ef9e9b1c018f85a8fb10ded4c4f55a791
pangyuteng <pangyuteng@gmail.com>
2025-02-13 17:46:43 +0000

    aded cli for utils/delete_postgres.py

--- flask/utils/delete_postgres.py
+++ flask/utils/delete_postgres.py
@@ -6,9 +6,21 @@ from .postgres_utils import (
 
 if __name__ == "__main__":
     daystamp = sys.argv[1]
-
-    table_list = 'timeandsale,summary,quote,greeks,event,candle,profile,theoprice,trade,underlying'.split(",")
-    #table_list = 'event_agg,gex_strike,gex_net'.split(",")
+    table_kind = sys.argv[2]
+    
+    events_list = 'timeandsale,summary,quote,greeks,event,candle,profile,theoprice,trade,underlying'.split(",")
+    gex_list = 'event_agg,gex_strike,gex_net'.split(",")
+    table_list = []
+    if table_kind == "all":
+        table_list.extend(events_list)
+        table_list.extend(gex_list)
+    elif table_kind == "events":
+        table_list.extend(events_list)
+    elif table_kind == "gex":
+        table_list.extend(gex_list)
+    else:
+        raise NotImplementedError()
+    
     for tablestr in table_list:
         query_str = "delete from "+tablestr+" where tstamp < %s"
         query_args = (daystamp,)
@@ -18,6 +30,6 @@ if __name__ == "__main__":
 
 """
 
-python -m utils.delete_postgres 2025-02-11
+python -m utils.delete_postgres 2025-02-14 gex
 
 """
