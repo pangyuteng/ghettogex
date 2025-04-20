7d6130b03d2dc81b039225e77e5f7dabdb8559a8
pangyuteng <pangyuteng@gmail.com>
2025-04-20 16:06:31 +0000

    wip

---
+++ scratch/uw_gex_utils.py
@@ -0,0 +1,75 @@
+
+import os
+import sys
+import datetime
+import pathlib
+import tempfile
+import zipfile
+from tqdm import tqdm
+import pandas as pd
+
+BOT_EOD_ROOT = "/mnt/hd2/data/finance/bot-eod-zip"
+CACHE_FOLDER = "/mnt/hd1/data/uw-options-cache"
+
+class GexService(object):
+    def __init__(self,ticker):
+        self.ticker = ticker
+        self.zip_file_list = None
+
+    def _prepare(self):
+        # save option flow parquet file.
+        self.zip_file_list = sorted(str(x) for x in pathlib.Path(BOT_EOD_ROOT).rglob("*.zip"))
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            mylist = []
+            for zip_file in tqdm(self.zip_file_list):
+                
+                tstamp_from_file = os.path.basename(zip_file).replace("bot-eod-report-","").replace(".zip","")
+                pq_file = os.path.join(CACHE_FOLDER,self.ticker,f"{tstamp_from_file}.parquet.gzip")
+
+                print(pq_file)
+                print('reading',zip_file)
+
+                if self.ticker == 'SPX':
+                    symbol = 'SPXW'
+                elif self.ticker == 'NDX':
+                    symbol = 'NDXP'
+                else:
+                    symbol = self.ticker
+
+                archive = zipfile.ZipFile(zip_file, 'r')
+                csv_file = os.path.basename(zip_file).replace(".zip",".csv")
+                with archive.open(csv_file) as f:
+                    df = pd.read_csv(f,low_memory=False)
+                    df = df[df.underlying_symbol==symbol]
+                    df['tstamp'] = df.executed_at.apply(lambda x: format_stamp(x))
+                    df['tstamp_sec'] = df.tstamp.apply(lambda x: x.replace(microsecond=0))
+
+                    df.to_parquet(pq_file,compression='gzip')
+
+        # every day. get 
+        #SPX,SPXW
+        #NDX,NDXP
+
+    def get_gex_detailed(tstamp):
+        _process()
+        pass
+    def get_gex_total(self,tstamp):
+        df = get_gex_detailed(tstamp)
+        return df.gex.sum()
+
+if __name__ == "__main__":
+    ticker = sys.argv[1]
+    gs = GexService(ticker)
+    gs._prepare()
+
+
+"""
+
+util to get gex from UW option flow data zip file.
+
+docker run -it -w $PWD -v /mnt:/mnt -p 8888:8888 fi-notebook:latest bash
+
+python uw_gex_utils.py SPX
+
+"""
