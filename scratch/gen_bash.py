2c9ebfe365b9decad7c0d0cab8b464a440c62f79
pangyuteng <pangyuteng@gmail.com>
2025-05-13 05:52:06 +0000

    wip batch

--- scratch/gen_bash.py
+++ scratch/gen_bash.py
@@ -5,7 +5,7 @@ market_close = datetime.datetime(2025,5,12)
 mylist = pd.date_range(start=market_open,end=market_close,freq='d')
 for item in mylist:
     tstamp = item.strftime('%Y-%m-%d')
-    print(f"python uw_gex_utils.py {tstamp}")
+    print(f"python uw_gex_utils.py SPX {tstamp}")
 
 
 """
