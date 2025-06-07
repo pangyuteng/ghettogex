f0b4fe3845ceecf028b354526f8685db01a0adc8
pangyuteng <pangyuteng@gmail.com>
2025-06-07 19:37:24 +0000

    updated trigger_cache_cboe schedule to run 30 min prior marketopen

--- flask/celeryconfig.py
+++ flask/celeryconfig.py
@@ -53,7 +53,7 @@ beat_schedule = {
    },
    'trigger_cache_cboe': {
        'task': 'tasks.trigger_cache_cboe',
-       'schedule': crontab(minute=1, hour=0), # utc midnight, ~7pm et
+       'schedule': crontab(minute=30,hour=13,day_of_week='mon,tue,wed,thu,fri')), # utc 13:30, 8:30ET
        'options': {'queue': 'default'},
        'args': [],
    },
