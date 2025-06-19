527c14de2516f0d13d44fc3654e628abfeeb0b84
pangyuteng <pangyuteng@gmail.com>
2025-06-19 03:56:54 +0000

    wip

---
+++ scratch/psycopg2-memory-leak-issue/create.py
@@ -0,0 +1,76 @@
+
+import logging
+logger = logging.getLogger(__file__)
+
+import datetime
+import os
+import sys
+import traceback
+import psycopg
+import psycopg_pool
+from psycopg.rows import dict_row
+
+postgres_uri = os.environ.get("POSTGRES_URI")
+
+def postgres_execute(query_str,query_args,is_commit=False):
+    response = None
+    try:
+        with psycopg.connect(postgres_uri,autocommit=True,row_factory=dict_row) as conn:
+            with conn.cursor() as curs:
+                curs.execute(query_str,query_args)
+                if is_commit is False:
+                    response = curs.fetchall()
+    except:
+        traceback.print_exc()
+    return response
+
+
+insert_query =  """
+
+CREATE TABLE IF NOT EXISTS gex_strike (
+    gex_strike_id SERIAL,
+    ticker text,
+    tstamp TIMESTAMP,
+    strike double precision,
+    volume_gex double precision,
+    state_gex double precision,
+    dex double precision,
+    convexity double precision,
+    vex double precision,
+    cex double precision,
+    call_convexity double precision,
+    call_oi double precision,
+    call_dex double precision,
+    call_gex double precision,
+    call_vex double precision,
+    call_cex double precision,
+    put_convexity double precision,
+    put_oi double precision,
+    put_dex double precision,
+    put_gex double precision,
+    put_vex double precision,
+    put_cex double precision,
+    UNIQUE (ticker, tstamp, strike)
+) WITH (
+  tsdb.hypertable=true,
+  tsdb.partition_column='tstamp',
+  tsdb.segmentby='ticker',
+  tsdb.orderby='tstamp DESC'
+);
+
+CALL add_columnstore_policy('gex_strike', after => INTERVAL '1d');
+
+create index gex_strike_tstamp_ticker_index on gex_strike using brin (tstamp,ticker) WITH (timescaledb.transaction_per_chunk);
+
+"""
+
+if __name__ == "__main__":
+    print("ok")
+    postgres_execute(insert_query,(),True)
+    print("done")
+
+"""
+
+export POSTGRES_URI=postgres://postgres:password@192.168.68.143:5432/postgres
+
+"""
---
+++ scratch/psycopg2-memory-leak-issue/insert.py
@@ -0,0 +1,69 @@
+import os
+import asyncio
+import traceback
+import datetime
+import pandas as pd
+import numpy as np
+
+import psycopg
+import psycopg_pool
+from psycopg.rows import dict_row
+
+async def apostgres_execute_many(apool,query_dict):
+    response = None
+    try:
+        await apool.check()
+        async with apool.connection() as aconn:
+            async with aconn.cursor(row_factory=dict_row) as curs:
+                for query_str,query_list in query_dict.items():
+                    await curs.executemany(query_str,query_list)
+    except:
+        traceback.print_exc()
+    return response
+
+postgres_uri = os.environ.get("POSTGRES_URI")
+async def background_subscribe():
+    async with psycopg_pool.AsyncConnectionPool(postgres_uri,min_size=4,open=False) as apool:
+        while True:
+            query_dict = {}
+            tstamp = datetime.datetime.now()
+            gex_strike_query_str = """
+                INSERT INTO gex_strike (ticker,strike,tstamp,volume_gex,state_gex,dex,convexity,vex,cex,
+                call_convexity,call_oi,call_dex,call_gex,call_vex,call_cex,
+                put_convexity,put_oi,put_dex,put_gex,put_vex,put_cex
+                ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,
+                    %s,%s,%s,%s,%s,%s,
+                    %s,%s,%s,%s,%s,%s)
+                on conflict (ticker,strike,tstamp) do update set 
+                volume_gex = %s,state_gex = %s,dex = %s,convexity = %s,vex = %s,cex = %s,
+                call_convexity = %s,call_oi = %s,call_dex = %s,call_gex = %s,call_vex = %s,call_cex = %s,
+                put_convexity = %s,put_oi = %s,put_dex = %s,put_gex = %s,put_vex = %s,put_cex = %s
+            """
+            
+            cols = 'ticker,strike,tstamp,volume_gex,state_gex,dex,convexity,vex,cex,call_convexity,call_oi,call_dex,call_gex,call_vex,call_cex,put_convexity,put_oi,put_dex,put_gex,put_vex,put_cex'.split(",")
+            df = pd.DataFrame([],columns=cols)
+            df['ticker']=np.array(['SPX']*1000)
+            df['strike']=np.arange(0,1000)
+            df['tstamp']=tstamp
+            for field_name in cols:
+                if field_name in ['ticker','strike','tstamp']:
+                    continue
+                df[field_name]=np.random.rand(1000)
+
+            async def insert_gex_strike(row):
+                query_args = [row.ticker,row.strike,row.tstamp,row.volume_gex,row.state_gex,row.dex,row.convexity,row.vex,row.cex,
+                row.call_convexity,row.call_oi,row.call_dex,row.call_gex,row.call_vex,row.call_cex,
+                row.put_convexity,row.put_oi,row.put_dex,row.put_gex,row.put_vex,row.put_cex,
+                row.volume_gex,row.state_gex,row.dex,row.convexity,row.vex,row.cex,
+                row.call_convexity,row.call_oi,row.call_dex,row.call_gex,row.call_vex,row.call_cex,
+                row.put_convexity,row.put_oi,row.put_dex,row.put_gex,row.put_vex,row.put_cex]
+                return query_args
+
+            query_dict[gex_strike_query_str] = await asyncio.gather(*(insert_gex_strike(row) for n,row in df.iterrows()))
+            print(len(query_dict[gex_strike_query_str]))
+            await apostgres_execute_many(apool,query_dict)
+            print(df.head())
+            print(tstamp)
+
+if __name__ == "__main__":
+    asyncio.run(background_subscribe())
