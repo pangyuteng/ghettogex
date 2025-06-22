9443b7597e207f09e35f22d370ed4165c51f34dc
pangyuteng <pangyuteng@gmail.com>
2025-06-22 22:55:59 +0000

    added MATERIALIZED VIEW

---
+++ timescaledb/07-aggregate.sql
@@ -0,0 +1,20 @@
+
+
+
+CREATE MATERIALIZED VIEW get_net_1min WITH (timescaledb.continuous) AS
+SELECT time_bucket('1m', tstamp) as time, ticker, 
+    last(volume_gex,tstamp) as volume_ges, last(state_gex,tstamp) as state_gex, last(spot_price,tstamp) as spot_price
+FROM gex_net 
+GROUP BY time_bucket('1m', tstamp), ticker;
+
+SELECT add_continuous_aggregate_policy('get_net_1min',
+  start_offset => NULL,
+  end_offset => INTERVAL '1 min',
+  schedule_interval => INTERVAL '1 min');
+
+CALL refresh_continuous_aggregate('get_net_1min', NULL, NULL);
+
+
+-- DROP MATERIALIZED VIEW get_net_1min;
+-- SELECT remove_continuous_aggregate_policy('get_net_1min');
+-- https://gist.github.com/mathisve/3cf9fd3f97ba75ec4d20c483fd5016d2
