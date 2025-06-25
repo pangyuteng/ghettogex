db287d3bc38ef6bb01125f51584a4a0f6d929fc5
pangyuteng <pangyuteng@gmail.com>
2025-06-25 16:35:53 +0000

    testing redis.json

--- scratch/debug_redis.py
+++ scratch/debug_redis.py
@@ -1,5 +1,9 @@
+
 import os
-import redis
+import datetime
+import time
+import numpy as np
+import json
 import redis
 from redis.commands.json.path import Path
 
@@ -8,15 +12,73 @@ print(uri)
 
 r = redis.from_url(uri,decode_responses=True)
 r.ping()
+
+ticker = 'SPX'
+if False:
+    key_list = []
+    for contract_type in ['call','put']:
+        for strike in np.arange(1000,5000,5):
+            key = f'quote:{ticker}:{contract_type}:{strike}'
+            key_list.append(key)
+    for x in key_list:
+        r.delete(x)
+
+if False:
+    timea = time.time()
+    for contract_type in ['call','put']:
+        for strike in np.arange(1000,5000,5):
+            tstamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            key = f'quote:{ticker}:{contract_type}:{strike}'
+            value = {
+                'bid_price': float(np.random.rand()),
+                'ask_price': float(np.random.rand()),
+                'event_symbol': f'{ticker}:{contract_type}:{strike}',
+                'tstamp': tstamp
+            }
+            #res = r.hset(key, mapping=value)
+            r.json().set(key, Path.root_path(), value, decode_keys=True)
+    timeb = time.time()
+    print(timeb-timea)
+
+key_list = []
+for contract_type in ['call','put']:
+    for strike in np.arange(1000,5000,5):
+        key = f'quote:{ticker}:{contract_type}:{strike}'
+        key_list.append(key)
+
 if True:
-    myvalue = {'a':1,'b':'c'}
-    mykey = 'mykey:10-10-01'
-    r.json().set(mykey, Path.root_path(), myvalue)
-    myvalue = {'a':2,'b':'z'}
-    mykey = 'mykey:10-10-02'
-    r.json().set(mykey, Path.root_path(), myvalue)
-    res = r.delete("user:2")
+    timea = time.time()
+    contract_type = 'call'
+    strike = 1000
+    firstkey = f'quote:{ticker}:{contract_type}:{strike}'
+    res = r.json().get(firstkey)
     print(res)
-res = r.hscan("mykey:",cursor=0,match="*")
-print(res)
-# HSCAN myhash 0 MATCH order_*
+    timeb = time.time()
+    print(timeb-timea)
+
+if True:
+    timea = time.time()
+    res = r.json().mget(key_list,Path.root_path())
+    #print(res)
+    print(len(res))
+    timeb = time.time()
+    print(timeb-timea)
+
+
+# r.json().mget
+
+"""
+
+kubectl port-forward --address 0.0.0.0 svc/postgres -n gg 5432:5432
+kubectl port-forward --address 0.0.0.0 svc/redis -n gg 6379:6379
+
+docker run -it \
+-e CACHE_FOLDER="/mnt/hd1/data/fi" \
+-e CACHE_TASTY_FOLDER="/mnt/hd1/data/tastyfi" \
+-e POSTGRES_URI="postgres://postgres:postgres@192.168.68.143:5432/postgres" \
+-e REDIS_URI="redis://192.168.68.143:6379/1" \
+-w $PWD -v /mnt:/mnt \
+-p 80:80 -p 8888:8888 \
+fi-notebook:latest bash
+
+"""
