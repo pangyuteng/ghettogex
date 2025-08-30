63416af36770a572bded0d597006e1c3ee7b78d9
pangyuteng <pangyuteng@gmail.com>
2025-08-30 16:37:20 +0000

    unindent

--- timescaledb/00-create-watchlist.sql
+++ timescaledb/00-create-watchlist.sql
@@ -1,14 +1,14 @@
 
-    CREATE TABLE IF NOT EXISTS watchlist (
-        watchlist_id SERIAL PRIMARY KEY, 
-        ticker text UNIQUE,
-        compute_gex bool
-    );
-    INSERT INTO watchlist(ticker,compute_gex) VALUES('SPX',true);
-    INSERT INTO watchlist(ticker,compute_gex) VALUES('VIX',false);
-    
+CREATE TABLE IF NOT EXISTS watchlist (
+    watchlist_id SERIAL PRIMARY KEY,
+    ticker text UNIQUE,
+    compute_gex bool
+);
+INSERT INTO watchlist(ticker,compute_gex) VALUES('SPX',true);
+INSERT INTO watchlist(ticker,compute_gex) VALUES('VIX',false);
+
 
 CREATE TABLE IF NOT EXISTS session (
-    session_id SERIAL PRIMARY KEY, 
+    session_id SERIAL PRIMARY KEY,
     serialized_session text
 );
