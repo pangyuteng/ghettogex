46a80302c487ab09f14064d299fe3531a6f56e34
pangyuteng <pangyuteng@gmail.com>
2025-11-08 03:51:43 +0000

    add_retention_policy

--- .timescaledb/08-retention.sql
+++ .timescaledb/08-retention.sql
@@ -1,4 +1,21 @@
 
+SELECT remove_retention_policy('candle');
+SELECT remove_retention_policy('greeks');
+SELECT remove_retention_policy('summary');
+SELECT remove_retention_policy('timeandsale');
+SELECT remove_retention_policy('quote');
+
+SELECT remove_retention_policy('gex_strike');
+SELECT remove_retention_policy('gex_net');
+SELECT remove_retention_policy('event_agg');
+
+SELECT remove_retention_policy('gex_net_1min');
+SELECT remove_retention_policy('candle_1min');
+SELECT remove_retention_policy('order_imbalance');
+SELECT remove_retention_policy('quote_5min');
+SELECT remove_retention_policy('order_imbalance_1day');
+SELECT remove_retention_policy('greeks_1day');
+
 SELECT add_retention_policy('candle', INTERVAL '100 days');
 SELECT add_retention_policy('greeks', INTERVAL '100 days');
 SELECT add_retention_policy('summary', INTERVAL '100 days');
@@ -9,6 +26,10 @@ SELECT add_retention_policy('gex_strike', INTERVAL '100 days');
 SELECT add_retention_policy('gex_net', INTERVAL '100 days');
 SELECT add_retention_policy('event_agg', INTERVAL '100 days');
 
+SELECT add_retention_policy('gex_net_1min', INTERVAL '100 days');
 SELECT add_retention_policy('candle_1min', INTERVAL '100 days');
 SELECT add_retention_policy('order_imbalance', INTERVAL '100 days');
-SELECT add_retention_policy('', INTERVAL '100 days');
+SELECT add_retention_policy('quote_5min', INTERVAL '100 days');
+SELECT add_retention_policy('order_imbalance_1day', INTERVAL '100 days');
+SELECT add_retention_policy('greeks_1day', INTERVAL '100 days');
+
