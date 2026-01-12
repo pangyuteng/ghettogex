b9a14281cc471346f32dcfcf31feb9febc5df4fc
pangyuteng <pangyuteng@gmail.com>
2026-01-12 16:54:57 +0000

    patch 07-aggregate.sql

--- .timescaledb/08-retention.sql
+++ .timescaledb/08-retention.sql
@@ -1,22 +1,23 @@
 
-SELECT remove_retention_policy('candle');
-SELECT remove_retention_policy('greeks');
-SELECT remove_retention_policy('summary');
-SELECT remove_retention_policy('timeandsale');
-SELECT remove_retention_policy('quote');
+-- SELECT remove_retention_policy('candle');
+-- SELECT remove_retention_policy('greeks');
+-- SELECT remove_retention_policy('summary');
+-- SELECT remove_retention_policy('timeandsale');
+-- SELECT remove_retention_policy('quote');
 
-SELECT remove_retention_policy('event_contract');
-SELECT remove_retention_policy('event_strike');
-SELECT remove_retention_policy('event_underlying');
+-- SELECT remove_retention_policy('event_contract');
+-- SELECT remove_retention_policy('event_strike');
+-- SELECT remove_retention_policy('event_underlying');
 
-SELECT remove_retention_policy('event_underlying_1min');
-SELECT remove_retention_policy('candle_1min');
-SELECT remove_retention_policy('order_imbalance');
-SELECT remove_retention_policy('quote_1min');
-SELECT remove_retention_policy('quote_1day');
-SELECT remove_retention_policy('order_imbalance_1day');
-SELECT remove_retention_policy('greeks_1day');
-SELECT remove_retention_policy('greeksdx_1day');
+-- SELECT remove_retention_policy('event_underlying_1min');
+-- SELECT remove_retention_policy('event_strike_1min');
+-- SELECT remove_retention_policy('candle_1min');
+-- SELECT remove_retention_policy('order_imbalance');
+-- SELECT remove_retention_policy('quote_1min');
+-- SELECT remove_retention_policy('quote_1day');
+-- SELECT remove_retention_policy('order_imbalance_1day');
+-- SELECT remove_retention_policy('greeks_1day');
+-- SELECT remove_retention_policy('greeksdx_1day');
 
 SELECT add_retention_policy('candle', INTERVAL '100 days');
 SELECT add_retention_policy('greeks', INTERVAL '100 days');
@@ -29,6 +30,7 @@ SELECT add_retention_policy('event_strike', INTERVAL '100 days');
 SELECT add_retention_policy('event_underlying', INTERVAL '100 days');
 
 SELECT add_retention_policy('event_underlying_1min', INTERVAL '100 days');
+SELECT remove_retention_policy('event_underlying_1min', INTERVAL '100 days');
 SELECT add_retention_policy('candle_1min', INTERVAL '100 days');
 SELECT add_retention_policy('order_imbalance', INTERVAL '100 days');
 SELECT add_retention_policy('quote_1min', INTERVAL '100 days');
