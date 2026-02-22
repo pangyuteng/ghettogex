4a703e9eefef83d9cbcdee49462c265a632011de
pangyuteng <pangyuteng@gmail.com>
2026-02-22 16:54:12 +0000

    added table indices

--- .timescaledb/08-retention.sql
+++ .timescaledb/08-retention.sql
@@ -19,23 +19,22 @@
 -- SELECT remove_retention_policy('greeks_1day');
 -- SELECT remove_retention_policy('greeksdx_1day');
 
-SELECT add_retention_policy('candle', INTERVAL '100 days');
-SELECT add_retention_policy('greeks', INTERVAL '100 days');
-SELECT add_retention_policy('summary', INTERVAL '100 days');
-SELECT add_retention_policy('timeandsale', INTERVAL '100 days');
-SELECT add_retention_policy('quote', INTERVAL '100 days');
+SELECT add_retention_policy('candle', INTERVAL '20 days');
+SELECT add_retention_policy('greeks', INTERVAL '20 days');
+SELECT add_retention_policy('summary', INTERVAL '20 days');
+SELECT add_retention_policy('timeandsale', INTERVAL '20 days');
+SELECT add_retention_policy('quote', INTERVAL '20 days');
 
-SELECT add_retention_policy('event_contract', INTERVAL '100 days');
-SELECT add_retention_policy('event_strike', INTERVAL '100 days');
-SELECT add_retention_policy('event_underlying', INTERVAL '100 days');
+SELECT add_retention_policy('event_contract', INTERVAL '20 days');
+SELECT add_retention_policy('event_strike', INTERVAL '20 days');
+SELECT add_retention_policy('event_underlying', INTERVAL '20 days');
 
-SELECT add_retention_policy('event_underlying_1min', INTERVAL '100 days');
-SELECT add_retention_policy('event_strike_1min', INTERVAL '100 days');
-SELECT add_retention_policy('candle_1min', INTERVAL '100 days');
-SELECT add_retention_policy('order_imbalance', INTERVAL '100 days');
-SELECT add_retention_policy('quote_1min', INTERVAL '100 days');
-SELECT add_retention_policy('quote_1day', INTERVAL '100 days');
-SELECT add_retention_policy('order_imbalance_1day', INTERVAL '100 days');
-SELECT add_retention_policy('greeks_1day', INTERVAL '100 days');
-SELECT add_retention_policy('greeksdx_1day', INTERVAL '100 days');
-
+SELECT add_retention_policy('event_underlying_1min', INTERVAL '20 days');
+SELECT add_retention_policy('event_strike_1min', INTERVAL '20 days');
+SELECT add_retention_policy('candle_1min', INTERVAL '20 days');
+SELECT add_retention_policy('order_imbalance', INTERVAL '20 days');
+SELECT add_retention_policy('quote_1min', INTERVAL '20 days');
+SELECT add_retention_policy('quote_1day', INTERVAL '20 days');
+SELECT add_retention_policy('order_imbalance_1day', INTERVAL '20 days');
+SELECT add_retention_policy('greeks_1day', INTERVAL '20 days');
+SELECT add_retention_policy('greeksdx_1day', INTERVAL '20 days');
